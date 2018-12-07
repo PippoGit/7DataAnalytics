b80b12c4362d49c01c7743c7b70879966d7498bd
PippoGit <filipscotto@gmail.com>
2018-12-07 16:53:46 +0100

    Update parser.py

--- 7parser/parser.py
+++ 7parser/parser.py
@@ -1,8 +1,9 @@
 import csv
+#import time
 
 #parse function
 # input_file: csv source
-# output_file: modified file with fixed columns
+# output_file: modified file with fixed columns (should be sorted by timestamp)
 
 def parse(input_file, output_file):
     fieldnames = ["machine", "metric", "timestamp", "value"];
@@ -15,6 +16,11 @@ def parse(input_file, output_file):
         for row in reader:
             id, metric = row[0].split('.')
             rows.append([id, metric, row[1], row[2]])
+
+        #rows.sort(key=lambda x: time.mktime(time.strptime(x[2],"%Y-%m-%d %H:%M:%S")))
+        #i think this is kinda useless, timestamps are just ISO strings, so it should be possible to sort em only
+        #by doing dates.sort(), there is no need to parse date.
+        rows.sort(key=lambda x: x[2]) #as long as timestamps are standard strings, i just sort em as strings (alotfaster)
 
     with open(output_file, 'w+') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
