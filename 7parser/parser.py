72e333ebb60430658e1fd9916988a353b756dc2e
PippoGit <filipscotto@gmail.com>
2018-12-07 16:16:56 +0100

    Update parser.py

--- 7parser/parser.py
+++ 7parser/parser.py
@@ -1,18 +1,28 @@
 import csv
 
-def parse():
-    file = "csv/fplog.csv"
+#parse function
+# input_file: csv source
+# output_file: modified file with fixed columns
 
-    with open(file, 'rb') as csvFile:
-        spamreader = csv.reader(csvFile, delimiter=',', quotechar='"')
-        next(spamreader, None)
-        nrow = []
-        for row in spamreader:
-            print row[0]
+def parse(input_file, output_file):
+    fieldnames = ["machine", "metric", "timestamp", "value"];
+    rows = []
+
+    with open(input_file, 'rb') as csv_file:
+        reader = csv.reader(csv_file, delimiter=',', quotechar='"')
+        next(reader, None)
+
+        for row in reader:
             id, metric = row[0].split('.')
-            nrow.append([id, metric, row[1], row[2]])
-            print nrow
-            return
+            rows.append([id, metric, row[1], row[2]])
 
+    with open(output_file, 'w+') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for row in rows:
+            writer.writerow({"machine":row[0], "metric":row[1], "timestamp":row[2], "value":row[3]})
+
+# main
 if __name__ == '__main__':
-    parse()
+    parse("csv/fplog.csv", "csv/newlog.csv")
